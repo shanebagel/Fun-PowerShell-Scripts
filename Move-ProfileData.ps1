3411f7e65053793305237d6322fd85a6d4c0de70
shane <hartley.shane@outlook.com>
2023-08-29 15:10:29 -0400

    Updated script file

--- Move-ProfileData.ps1
+++ Move-ProfileData.ps1
@@ -1,4 +1,4 @@
-function Move-ProfileData {
+function Move-ProfileData { # Start function
 
 <#
 .SYNOPSIS 
@@ -18,23 +18,20 @@ Taskbar File location
 
 Optionally: Outlook Data Files - use the IncludeOutlookData paramater 
 
-.PARAMETER UserName
-Username of the account you're running script against
+.PARAMETER ComputerName
+Computer name of the machine that houses the profile data. Defaults to local hostname where script is ran from
 
 .PARAMETER NewProfileName
 New profile you're migrating data into
 
-.PARAMETER ComputerName
-Computer name of the machine that houses the profiles
-
-.PARAMETER ProfileName
+.PARAMETER OldProfileName
 Old profile you're migrating data out of 
 
 .PARAMETER IncludeOutlookData
 If $true is passed in outlook data files will be copied over to new profile. By default outlook data files will not be included in move
 
 .EXAMPLE
-Move-ProfileData -ComputerName "Shaneserver" -ProfileName "OLD-bob" -NewProfileName "bob" -Username "bob"
+Move-ProfileData -ComputerName "Shaneserver" -OldProfileName "oldprofile" -NewProfileName "newprofile" -IncludeOutlookData $False
 
 .NOTES
 Can be ran after Start-ProfileRebuild.ps1
@@ -43,25 +40,24 @@ Can be ran after Start-ProfileRebuild.ps1
 
 	[CmdletBinding()]
 	Param (
-		[Parameter(ValueFromPipeline = $True, Mandatory = $True)]
-		[String]$ComputerName,
+		[Parameter(ValueFromPipeline = $True, Mandatory = $False)]
+		[String]$ComputerName = $env:COMPUTERNAME,
 
 		[Parameter(Mandatory = $True)]
 		[String]$NewProfileName,
 
 		[Parameter(Mandatory = $True)]
-		[String]$ProfileName,
+		[String]$OldProfileName,
 
 		[Parameter(Mandatory = $True)]
-		[String]$UserName,
-
-		[Parameter(Mandatory = $False, ValidateNotNull)]
 		[Boolean]$IncludeOutlookData
 
 	) 
 
 	begin {
-		$TestCondition = (Test-Path "C:\Users\$ProfileName") -and (Test-Path "C:\Users\$NewProfileName")	
+		$SourceProfileExists = Test-Path "C:\Users\$OldProfileName"
+		$DestinationProfileExists = Test-Path "C:\Users\$NewProfileName"
+		$TestCondition = (($SourceProfileExists) -and ($DestinationProfileExists))	
 	}
 
 	process {
@@ -69,21 +65,21 @@ Can be ran after Start-ProfileRebuild.ps1
 		if ( $TestCondition ) {
 
 			$SourcePaths = @( # Source file locations
-				"C:\Users\$ProfileName\Desktop\*"
-				"C:\Users\$ProfileName\Downloads\*"
-				"C:\Users\$ProfileName\Documents\*"
-				"C:\Users\$ProfileName\Pictures\*"
-				"C:\Users\$ProfileName\Videos\*"
-				"C:\Users\$ProfileName\Appdata\Roaming\microsoft\windows\recent\automaticdestinations\*"
-				"C:\Users\$ProfileName\AppData\Local\Google\Chrome\User Data\Default\*"
-				"C:\Users\$ProfileName\AppData\Local\Microsoft\Edge\User Data\Default\*"
-				"C:\Users\$ProfileName\AppData\Roaming\Mozilla\Firefox\Profiles\*"
-				"C:\Users\$ProfileName\AppData\local\Mozilla\Firefox\Profiles\*"
-				"C:\Users\$ProfileName\AppData\Roaming\Microsoft\Signatures\*"
-				"C:\Users\$ProfileName\AppData\Local\Packages\Microsoft.Office.OneNote_8wekyb3d8bbwe\*"
-				"C:\Users\$ProfileName\AppData\Local\Microsoft\OneNote\16.0\Backup\*"
-				"C:\Users\$ProfileName\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*"
-				"C:\Users\$ProfileName\AppData\Local\Microsoft\Vault\*"
+				"C:\Users\$OldProfileName\Desktop\*"
+				"C:\Users\$OldProfileName\Downloads\*"
+				"C:\Users\$OldProfileName\Documents\*"
+				"C:\Users\$OldProfileName\Pictures\*"
+				"C:\Users\$OldProfileName\Videos\*"
+				"C:\Users\$OldProfileName\Appdata\Roaming\microsoft\windows\recent\automaticdestinations\*"
+				"C:\Users\$OldProfileName\AppData\Local\Google\Chrome\User Data\Default\*"
+				"C:\Users\$OldProfileName\AppData\Local\Microsoft\Edge\User Data\Default\*"
+				"C:\Users\$OldProfileName\AppData\Roaming\Mozilla\Firefox\Profiles\*"
+				"C:\Users\$OldProfileName\AppData\local\Mozilla\Firefox\Profiles\*"
+				"C:\Users\$OldProfileName\AppData\Roaming\Microsoft\Signatures\*"
+				"C:\Users\$OldProfileName\AppData\Local\Packages\Microsoft.Office.OneNote_8wekyb3d8bbwe\*"
+				"C:\Users\$OldProfileName\AppData\Local\Microsoft\OneNote\16.0\Backup\*"
+				"C:\Users\$OldProfileName\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\*"
+				"C:\Users\$OldProfileName\AppData\Local\Microsoft\Vault\*"
 			)
 
 			$DestinationPaths = @(
@@ -105,24 +101,49 @@ Can be ran after Start-ProfileRebuild.ps1
 			)
 
 			# Move-Item moves data into new profile programmatically 
-			foreach ( $SourcePath in $SourcePaths ) {
-				foreach ( $DestinationPath in $DestinationPaths ) {
-					Move-Item -Path $SourcePath -Destination $DestinationPath
+			for ($i = 0; $i -lt $SourcePaths.Length; $i++) {
+				$SourcePath = $SourcePaths[$i]
+				$DestinationPath = $DestinationPaths[$i]
+				if ((Test-Path -Path $SourcePath) -and (Test-Path -Path $DestinationPath)) {
+					Move-Item -Path $SourcePath -Destination $DestinationPath -Force # Move all content in directory to new profile directory
 				}
+				else {
+					Write-Warning "Skipping directory $SourcePath as source or destination directory doesn't exist" 
+				}
+			} # End for loop
+				
+		} # End if statement
+
+		elseif ( $TestCondition = $False ) {
+			
+			if ( $SourceProfileExists -eq $False ) {
+				Write-Warning "Error - Source profile path C:\Users\$OldProfileName doesn't exist"
+				Write-Warning "Double check the source profile name"
+			}
+			if ( $DestinationProfileExists -eq $False ) {
+				Write-Warning "Error - Destination profile path C:\Users\$NewProfileName doesn't exist"
+				Write-Warning "Re-run script after logging in with user credentials to create the new profile if it doesn't exist"
 			}
 
-			# Move-Item moves data for outlook data files if the $True boolean value is passed in via the IncludeOutlookData parameter
-			if ( $IncludeOutlookData -eq $True ) {
-				Move-Item -Path "C:\Users\$ProfileName\AppData\Local\Microsoft\Outlook\*" -Destination "C:\Users\$NewProfileName\AppData\Local\Microsoft\Outlook"
+		} # End elseif
+				
+		# Move-Item moves data for outlook data files if the $True boolean value is passed in via the IncludeOutlookData parameter
+		if ( $IncludeOutlookData -eq $True ) {
+			$OutlookSource = "C:\Users\$OldProfileName\AppData\Local\Microsoft\Outlook\*"
+			$OutlookDestination = "C:\Users\$NewProfileName\AppData\Local\Microsoft\Outlook"
+
+			$OutlookSourceExists = Test-Path -Path $OutlookSource
+			$OutlookDestinationExists = Test-Path -Path $OutlookDestination
+
+			if (($OutlookSourceExists) -and ($OutlookDestinationExists)) {
+				Move-Item -Path "C:\Users\$OldProfileName\AppData\Local\Microsoft\Outlook\*" -Destination "C:\Users\$NewProfileName\AppData\Local\Microsoft\Outlook" -Force
+			}
+			else {
+				Write-Warning "Directory skipped - Outlook OST source file path $OutlookSource or destination file path $OutlookDestination doesn't exist"
 			}
 
 		} # End if statement
-
-		else {
-			Write-Warning "Error - File path C:\Users\$ProfileName or C:\Users\$NewProfileName doesn't exist!"
-			Write-Warning "Re-run script after logging in with user credentials to create the new profile if it doesn't exist"
-		}
-
+	
 	} # End process block
-
-} # End function
+	
+}  # End function
